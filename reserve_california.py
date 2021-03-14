162c516723a39fd4480cbac1a1bf401dc9656e40
samholladay <sam.holladay@berkeley.edu>
2021-03-13 21:00:00 -0800

    First draft

---
+++ reserve_california.py
@@ -0,0 +1,8 @@
+from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
+
+opts = Options()
+opts.set_headless()
+assert opts.headless  # Operating in headless mode
+browser = Firefox(options=opts)
+browser.get('https://reservecalifornia.com/CaliforniaWebHome/')
